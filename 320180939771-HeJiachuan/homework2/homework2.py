5b180b51051637173bcbbc93f5ff94372c21fcc0
Jiachuan_He <1213951268@qq.com>
2020-06-22 16:05:59 +0800

    I will finish homework2 before this Friday.

---
+++ 320180939771-HeJiachuan/homework2/homework2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+__author__ = "Jiachuan He - 320180939771"
+__copyright__ = "Copyright 2020, Jiachuan_He homework2"
+__time__ = "2020-6-1"
+__version__ = "1.0.0"
+__title__ = "homework2"
+__email__ = "hejch2018@lzu.edu.cn"
+__status__ = "Production"
+"""
+
+
+# This is just a empty PY file. And I will finish it before This Friday.
